4db87226db4b2897069340655062dd0bb4df7d80
Gessica <santos.gessicacarolina@gmail.com>
2023-04-23 12:56:41 -0300

    adjusting code for deploy

--- src/app.js
+++ src/app.js
@@ -12,4 +12,6 @@ app.use(express.json());
 app.use(authRouter);
 app.use(userRouter);
 
-app.listen(5000, () => console.log('rodandooo'))
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
